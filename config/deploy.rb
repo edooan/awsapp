52a39e3e2db95253c8c99b26358bb11b09f51cac
Edward Parra <gk.edooan@gmail.com>
2020-01-20 17:15:34 -0500

    fixed deploy path

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:edooan/awsapp.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/#{fetch :application}"
+set :deploy_to, "/home/ubuntu/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
